a5b23492b719118aaab088db0329ab901f0be1e8
flash106 <harikrishna.2992005@gmail.com>
2026-06-23 23:09:16 +0530

    update

--- main.py
+++ main.py
@@ -201,7 +201,17 @@ def main():
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            rtc_configuration={
+                 "iceServers": [
+                    {"urls": ["stun:stun.l.google.com:19302"]},  # STUN
+                    {
+                            "urls": "turn:turn.metered.ca:80",
+                            "username": "f6fdcd6f354ed44a3da13ac6",
+                            "credential": "yAWfBFfaEpBQqluq"
+                    }
+                    ]
+                }
+            # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
             media_stream_constraints={
                 "video": True,
                 "audio": False
