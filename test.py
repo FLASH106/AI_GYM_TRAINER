bef4f8991bb1736fa355ea42da21d2cd33f86fef
flash106 <harikrishna.2992005@gmail.com>
2026-06-26 15:48:52 +0530

    webrtctest1

--- test.py
+++ test.py
@@ -1,14 +1,34 @@
+# import streamlit as st
+# from streamlit_webrtc import webrtc_streamer
+
+# st.set_page_config(page_title="WebRTC Test")
+
+# st.title("🎥 WebRTC Camera Test")
+
+# webrtc_streamer(
+#     key="test",
+#     media_stream_constraints={
+#         "video": True,
+#         "audio": False,
+#     }
+# )
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
 
-st.set_page_config(page_title="WebRTC Test")
+st.title("WebRTC Test")
 
-st.title("🎥 WebRTC Camera Test")
-
-webrtc_streamer(
+ctx = webrtc_streamer(
     key="test",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]}
+        ]
+    },
     media_stream_constraints={
         "video": True,
         "audio": False,
-    }
+    },
 )
+
+st.write(ctx.state)
