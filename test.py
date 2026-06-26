35012834679a5df7daa7db9e9e477f3a9571f27f
flash106 <harikrishna.2992005@gmail.com>
2026-06-26 16:00:41 +0530

    webrtctest1

--- test.py
+++ test.py
@@ -1,34 +1,46 @@
+# # import streamlit as st
+# # from streamlit_webrtc import webrtc_streamer
+
+# # st.set_page_config(page_title="WebRTC Test")
+
+# # st.title("🎥 WebRTC Camera Test")
+
+# # webrtc_streamer(
+# #     key="test",
+# #     media_stream_constraints={
+# #         "video": True,
+# #         "audio": False,
+# #     }
+# # )
 # import streamlit as st
-# from streamlit_webrtc import webrtc_streamer
+# from streamlit_webrtc import webrtc_streamer, WebRtcMode
 
-# st.set_page_config(page_title="WebRTC Test")
+# st.title("WebRTC Test")
 
-# st.title("🎥 WebRTC Camera Test")
-
-# webrtc_streamer(
+# ctx = webrtc_streamer(
 #     key="test",
+#     mode=WebRtcMode.SENDRECV,
+#     rtc_configuration={
+#         "iceServers": [
+#             {"urls": ["stun:stun.l.google.com:19302"]}
+#         ]
+#     },
 #     media_stream_constraints={
 #         "video": True,
 #         "audio": False,
-#     }
+#     },
 # )
+
+# st.write(ctx.state)
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
+from streamlit_webrtc import webrtc_streamer
 
 st.title("WebRTC Test")
 
-ctx = webrtc_streamer(
+webrtc_streamer(
     key="test",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration={
-        "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]}
-        ]
-    },
     media_stream_constraints={
         "video": True,
         "audio": False,
     },
 )
-
-st.write(ctx.state)
